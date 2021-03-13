e55b39af67eb5f27ff82b282b8b039adcbcb03f2
cocoalex00 <alejandrodj5@hotmail.com>
2021-03-13 20:37:29 +0000

    Datasets separated in AD pos and neg

--- Code/Tests/Feature selection/fsNonParallelisedGA.py
+++ Code/Tests/Feature selection/fsNonParallelisedGA.py
@@ -1,3 +1,23 @@
 #!usr/bin/python3
 
-#This is an raw first implementation of the feature selection method using a binary coded genetic algorithm 
+#This is a raw first implementation of the feature selection method using a binary coded genetic algorithm, the parallelised version will be based on this.
+
+import pandas as pd
+from pathlib import Path
+
+#loading both datasets into dataframes 
+GeneExpression = pd.read_table('allforDNN_ge_sample.tsv')
+DNAmethylation = pd.read_table('allforDNN_me_sample.tsv')
+
+
+#Separating AD positive and negative samples for merging
+
+GEpos = GeneExpression.where(GeneExpression["Label_AD"] == 1).dropna(0,'all')
+GEneg = GeneExpression.where(GeneExpression["Label_AD"] == 0).dropna(0,'all')
+
+DNApos = DNAmethylation.where(DNAmethylation["Label_AD"] == 1).dropna(0,'all')
+DNAneg = DNAmethylation.where(DNAmethylation["Label_AD"] == 0).dropna(0,'all')
+
+print(GEpos)
+print(GEneg)
+print(GeneExpression)
