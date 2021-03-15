3115a0b94a42e332903a0b22d4aecbd06ad92cf0
cocoalex00 <alejandrodj5@hotmail.com>
2021-03-15 00:52:52 +0000

    Added function that transform the feature vector into the desired dataset (Feature selection)

---
+++ Code/Tests/FeatureSelection/NonParalellGA.py
@@ -0,0 +1,89 @@
+#This is a test implementation of the Genetic algorithm that will perform the feature selection. In this implementation, only the training of the models will be performed in the GPU, the GA itself won't be parallel as that will be implemented based on this verion.
+
+import numpy as np 
+import random
+import torch 
+import deap
+from deap import base, tools, creator
+import pandas as pd
+
+#Import Train and Test datasets
+Train = pd.read_csv(r'TrainDataset.csv') 
+Test = pd.read_csv(r'TestDataset.csv') 
+#Delete first column as it is unnecessary 
+
+Train.drop(Train.columns[0],axis = 1, inplace = True)
+Test.drop(Test.columns[0],axis = 1, inplace = True)
+
+#Convert the dataframes to numpy arrays in order to use them as input for the model
+
+y_train = Train.iloc[:,700:].to_numpy()
+
+y_test = Test.iloc[:,700:].to_numpy()
+
+
+
+
+
+
+#First we create the fitness atribute and the individual class 
+creator.create("Fitness", base.Fitness, weights = (1.0,))
+creator.create("Individual", list, fitness = creator.Fitness)
+
+#Then we create the evolutionary tools and the toolbox to contain them 
+toolbox = base.Toolbox()
+
+toolbox.register("random", random.randint, 0,1)
+toolbox.register("Individual", tools.initRepeat, creator.Individual, toolbox.random, 700)
+
+
+
+
+
+
+
+
+
+
+#This helper function decodes a chromosome (feature vector) and returns the elected columns of the dataframes converted to numpy arrays (for training/testing purposes)
+
+def decodeChromosome(FeatureVector):
+  #create a copy of the training/testing dataframes  
+  NewTrain = Train.iloc[:,:700].copy()
+  NewTest = Test.iloc[:,:700].copy()
+  
+
+  IndNonElected = []
+  #traverse the feature vector anotating the indexes not elected
+  for i in range(len(FeatureVector) - 1):
+    if FeatureVector[i] == 0:
+      IndNonElected.append(i)
+
+  #drop the features not selected in the vector from both train and thest dataset
+  NewTrain.drop( NewTrain.columns[IndNonElected] ,axis = 1, inplace = True)
+  NewTest.drop( NewTest.columns[IndNonElected], axis = 1, inplace = True)
+  return NewTrain.to_numpy(), NewTest.to_numpy()
+
+
+
+    
+  
+  
+
+#Main method
+def main():
+  
+  ind = toolbox.Individual()
+  print(ind)
+  x_train, x_test, = decodeChromosome(ind)
+  print(x_test.shape)
+
+if __name__ == "__main__":
+  main()
+
+  
+#print(x_train)
+#print(y_train)
+#print(x_test)
+#print(y_test)
+#print(Test)
